9dea81f54592b461291aa6fd1583d0b4733c6ed8
Leonardo J. O. Centurion Diaz <95662867+Arcanot@users.noreply.github.com>
2024-04-08 11:27:17 -0500

    Add files via upload

---
+++ juegos/adivinacolor/script.js
@@ -0,0 +1,92 @@
+//cargo en un arreglo las imganes de los colores. Este sera el orden que se mostrarán
+let colores = ["ba.png", "ca.png", "ci.png", "ki.png", "ve.png"];
+
+//arreglo que guardara la opcion correcta
+let correcta = [0,2,1,0,2];
+
+//arreglo que guardara las respuestas a mostrar en cada jugada
+let opciones = [];
+//cargo en el arreglo opciones las opciones a mostrar en cada jugada
+opciones.push(["AMARILLO", "PÚRPURA", "ROJO"]);
+opciones.push(["NEGRO", "AZUL", "MARRÓN"]);
+opciones.push(["TURQUEZA", "CELESTE", "AMARILLO"]);
+opciones.push(["ROSADO", "VERDE", "GRIS"]);
+opciones.push(["ANARANJADO", "BLANCO", "VERDE"]);
+
+//variable que guarda la posicion actual
+let posActual = 0;
+//variable que guarda la cantidad acertadas hasta el moemento
+let cantidadAcertadas = 0;
+
+function comenzarJuego(){
+    //reseteamos las variables
+    posActual = 0;
+    cantidadAcertadas = 0;
+    //activamos las pantallas necesarias
+    document.getElementById("pantalla-inicial").style.display = "none";
+    document.getElementById("pantalla-juego").style.display = "block";
+    cargarColor();
+
+}
+
+//funcion que carga el siguiente color y sus opciones
+function cargarColor(){
+    //controlo sis se acabaron los colores
+    if(colores.length <= posActual){
+        terminarJuego();
+    }
+    else{//cargo las opciones
+        //limpiamos las clases que se asignaron
+        limpiarOpciones();
+
+        document.getElementById("imgColor").src = "img/" + colores[posActual];
+        document.getElementById("n0").innerHTML = opciones[posActual][0];
+        document.getElementById("n1").innerHTML = opciones[posActual][1];
+        document.getElementById("n2").innerHTML = opciones[posActual][2];
+    }
+}
+
+function limpiarOpciones(){
+    document.getElementById("n0").className = "nombre";
+    document.getElementById("n1").className = "nombre";
+    document.getElementById("n2").className = "nombre";
+
+    document.getElementById("l0").className = "letra";
+    document.getElementById("l1").className = "letra";
+    document.getElementById("l2").className = "letra";
+}
+
+function comprobarRespuesta(opElegida){
+    if(opElegida==correcta[posActual]){//acertó
+        //agregamos las clases para colocar el color verde a la opcion elegida
+        document.getElementById("n" + opElegida).className = "nombre nombreAcertada";
+        document.getElementById("l" + opElegida).className = "letra letraAcertada";
+        cantidadAcertadas++;
+    }else{//no acerto
+        //agramos las clases para colocar en rojo la opcion elegida
+        document.getElementById("n" + opElegida).className = "nombre nombreNoAcertada";
+        document.getElementById("l" + opElegida).className = "letra letraNoAcertada";
+
+        //opcion que era correcta
+        document.getElementById("n" + correcta[posActual]).className = "nombre nombreAcertada";
+        document.getElementById("l" + correcta[posActual]).className = "letra letraAcertada";
+    }
+    posActual++;
+    //Esperamos 1 segundo y pasamos mostrar el siguiente color y sus opciones
+    setTimeout(cargarColor,1000);
+}
+function terminarJuego(){
+    //ocultamos las pantallas y mostramos la pantalla final
+    document.getElementById("pantalla-juego").style.display = "none";
+    document.getElementById("pantalla-final").style.display = "block";
+    //agreamos los resultados
+    document.getElementById("numCorrectas").innerHTML = cantidadAcertadas;
+    document.getElementById("numIncorrectas").innerHTML = colores.length - cantidadAcertadas;
+}
+
+function volverAlInicio(){
+    //ocultamos las pantallas y activamos la inicial
+    document.getElementById("pantalla-final").style.display = "none";
+    document.getElementById("pantalla-inicial").style.display = "block";
+    document.getElementById("pantalla-juego").style.display = "none";
+}
---
+++ juegos/memograma/JMemoria/Facil/JuegoDeMemoria.js
@@ -0,0 +1,100 @@
+
+let tarjetaDestapadas=0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null;
+let movimientos = 0;
+let aciertos = 0;
+let timer = 60;
+let tiempoInicial = 20;
+let temporizador = false;
+let tiempoRegresivoId = null;
+
+
+let mostrarmovimiento = document.getElementById('movimientos');
+let mostraraciertos = document.getElementById('aciertos');
+let mostrartiempo =document.getElementById('t-restante');
+
+let correcto = new Audio('Audio/Correcto.wav');
+let Error = new Audio('Audio/Error.wav');
+let gano = new Audio('Audio/sfx-victory1.mp3');
+let fondo = new Audio('Audio/fondo.wav');
+
+const Emergente_container = document.getElementById('Emergente_container');
+const cerrar = document.getElementById('cerrar');
+
+let numeros =[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+numeros = numeros.sort(()=>{return Math.random()-0.5});
+console.log(numeros);
+
+
+function bloqueartarjeta(){
+    for(let i=0;i<=15;i++){
+        let tarjetabloqueada =document.getElementById(i);
+        tarjetabloqueada.innerHTML=`<img src="JuegoDeMemoria/${numeros[i]}.png">`;
+        tarjetabloqueada.disabled = true;
+    }
+}
+
+
+function contarTiempo(){
+    tiempoRegresivoId = setInterval(()=>{
+        timer--;
+        mostrartiempo.innerHTML = `Tiempo:  ${timer} segundos`;
+        if(timer == 0){
+            clearInterval(tiempoRegresivoId);
+            bloqueartarjeta();
+        }
+    },1000)
+}
+
+function destapar(id){
+
+    if(temporizador == false){
+        contarTiempo();
+        temporizador = true;
+    }
+
+    tarjetaDestapadas++;
+    console.log(tarjetaDestapadas);
+    if(tarjetaDestapadas==1){
+        tarjeta1=document.getElementById(id);
+        primerResultado=numeros[id]
+        tarjeta1.innerHTML = `<img src="JuegoDeMemoria/${primerResultado}.png">`;
+        tarjeta1.disabled = true;
+    }else if(tarjetaDestapadas==2){
+        tarjeta2=document.getElementById(id);
+        segundoResultado=numeros[id];
+        tarjeta2.innerHTML=`<img src="JuegoDeMemoria/${segundoResultado}.png">`;
+        tarjeta2.disabled =true;
+        movimientos++;
+        mostrarmovimiento.innerHTML= `Movimientos:  ${movimientos}`;
+        if(primerResultado==segundoResultado){
+            tarjetaDestapadas=0;
+            aciertos++;
+            mostraraciertos.innerHTML = `Aciertos:  ${aciertos}`;
+            correcto.play();
+            if(aciertos == 8){
+                gano.play();
+                clearInterval(tiempoRegresivoId);
+                mostraraciertos.innerHTML= `Aciertos:  ${aciertos} WOW`;
+                mostrartiempo.innerHTML=`Tiempo:  ${tiempoInicial-timer} segundos.`
+                mostrarmovimiento.innerHTML= `Movimientos:  ${movimientos} WOW`;
+                Emergente_container.classList.add('show');  
+            }
+        }else{
+            Error.play();
+            setTimeout(()=>{
+                tarjeta1.innerHTML=' ';
+                tarjeta2.innerHTML=' ';
+                tarjeta1.disabled= false;
+                tarjeta2.disabled= false;
+                tarjetaDestapadas=0;
+            },800);
+        }
+    }
+}
+cerrar.addEventListener('click', () => {
+    Emergente_container.classList.remove('show');
+});
